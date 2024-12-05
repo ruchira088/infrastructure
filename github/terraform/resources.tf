643e6b74fb4cb652f611c21ef444b666dc6595e9
Ruchira (Richie) Jayasekara <me@ruchij.com>
2024-12-05 20:06:04 +1100

    Setting up GitHub as a OIDC provider in AWS

--- github/terraform/resources.tf
+++ github/terraform/resources.tf
@@ -1,24 +1,44 @@
-resource "aws_iam_user" "github_actions" {
-  name = "github-actions"
+
+resource "aws_iam_openid_connect_provider" "github_identity_provider" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
+  ]
 }
 
-resource "aws_iam_user_policy_attachment" "github_actions_policy_attachment" {
+resource "aws_iam_role" "github_iam_role" {
+  name = "github_iam_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github_identity_provider.arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
+          },
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" : "repo:ruchira088/*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-  user       = aws_iam_user.github_actions.name
+  role       = aws_iam_role.github_iam_role.name
 }
 
-resource "aws_iam_access_key" "github_actions_access_key" {
-  user = aws_iam_user.github_actions.name
-}
-
-resource "aws_ssm_parameter" "github_actions_access_key_id" {
-  name  = "/github/aws/access-key-id"
-  type  = "SecureString"
-  value = aws_iam_access_key.github_actions_access_key.id
-}
-
-resource "aws_ssm_parameter" "github_actions_secret_access_key" {
-  name  = "/github/aws/secret-access-key"
-  type  = "SecureString"
-  value = aws_iam_access_key.github_actions_access_key.secret
+output "result" {
+  value = aws_iam_role.github_iam_role.arn
 }
