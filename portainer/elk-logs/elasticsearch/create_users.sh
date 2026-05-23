720df8b9812f6ffc56dfd89288fb6160176d6e6a
Ruchira (Richie) Jayasekara <me@ruchij.com>
2026-05-24 02:33:20 +1000

    Update ELK stack: add memory limits, logging options, and data volume; improve health checks, indexing, and user roles configuration

--- portainer/elk-logs/elasticsearch/create_users.sh
+++ portainer/elk-logs/elasticsearch/create_users.sh
@@ -1,19 +1,34 @@
 #!/usr/bin/env sh
+set -eu
 
-curl -s -X POST \
+curl -s --fail-with-body -X PUT \
+  "$ELASTICSEARCH_HOST/_security/role/logstash_writer" \
+  -u "$ELASTICSEARCH_USERNAME:$ELASTICSEARCH_PASSWORD" \
+  -H "Content-Type: application/json" \
+  --data '{
+    "cluster": [ "monitor", "manage_index_templates" ],
+    "indices": [
+      {
+        "names": [ "logs*" ],
+        "privileges": [ "create_index", "create", "index", "write", "view_index_metadata" ]
+      }
+    ]
+  }'
+
+curl -s --fail-with-body -X POST \
   "$ELASTICSEARCH_HOST/_security/user/$KIBANA_USERNAME" \
   -u "$ELASTICSEARCH_USERNAME:$ELASTICSEARCH_PASSWORD" \
   -H "Content-Type: application/json" \
   --data "{
     \"password\" : \"$KIBANA_PASSWORD\",
-    \"roles\" : [ \"superuser\", \"kibana_admin\", \"kibana_system\" ]
+    \"roles\" : [ \"kibana_system\" ]
   }"
 
-curl -s -X POST \
+curl -s --fail-with-body -X POST \
   "$ELASTICSEARCH_HOST/_security/user/$LOGSTASH_USERNAME" \
   -u "$ELASTICSEARCH_USERNAME:$ELASTICSEARCH_PASSWORD" \
   -H "Content-Type: application/json" \
   --data "{
     \"password\" : \"$LOGSTASH_PASSWORD\",
-    \"roles\" : [ \"superuser\" ]
+    \"roles\" : [ \"logstash_writer\" ]
   }"
