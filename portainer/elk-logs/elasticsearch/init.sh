33175d95313112e33febd0d007864277305b1250
Ruchira (Richie) Jayasekara <me@ruchij.com>
2026-05-24 02:38:08 +1000

    Update ELK stack: rename user creation script, add log retention policy, and update Docker Compose dependencies

--- portainer/elk-logs/elasticsearch/init.sh
+++ portainer/elk-logs/elasticsearch/init.sh
@@ -1,6 +1,43 @@
 #!/usr/bin/env sh
 set -eu
 
+LOG_RETENTION_DAYS="${LOG_RETENTION_DAYS:-30}"
+
+curl -s --fail-with-body -X PUT \
+  "$ELASTICSEARCH_HOST/_ilm/policy/logs_policy" \
+  -u "$ELASTICSEARCH_USERNAME:$ELASTICSEARCH_PASSWORD" \
+  -H "Content-Type: application/json" \
+  --data "{
+    \"policy\": {
+      \"phases\": {
+        \"hot\": {
+          \"min_age\": \"0ms\",
+          \"actions\": {}
+        },
+        \"delete\": {
+          \"min_age\": \"${LOG_RETENTION_DAYS}d\",
+          \"actions\": { \"delete\": {} }
+        }
+      }
+    }
+  }"
+
+curl -s --fail-with-body -X PUT \
+  "$ELASTICSEARCH_HOST/_index_template/logs_template" \
+  -u "$ELASTICSEARCH_USERNAME:$ELASTICSEARCH_PASSWORD" \
+  -H "Content-Type: application/json" \
+  --data '{
+    "index_patterns": [ "logs-*" ],
+    "priority": 100,
+    "template": {
+      "settings": {
+        "index.lifecycle.name": "logs_policy",
+        "number_of_shards": 1,
+        "number_of_replicas": 0
+      }
+    }
+  }'
+
 curl -s --fail-with-body -X PUT \
   "$ELASTICSEARCH_HOST/_security/role/logstash_writer" \
   -u "$ELASTICSEARCH_USERNAME:$ELASTICSEARCH_PASSWORD" \
