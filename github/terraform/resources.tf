c354b97aa140b31340cf70285900c394543e407b
Ruchira (Richie) Jayasekara <me@ruchij.com>
2026-07-27 14:13:26 +1000

    Update GitHub Actions OIDC sub claim to include additional repository pattern
    Expand the `StringLike` condition under the `sub` claim to authorize actions from `repo:ruchira088@5546272/*` alongside `repo:ruchira088/*`.

--- github/terraform/resources.tf
+++ github/terraform/resources.tf
@@ -26,7 +26,10 @@ resource "aws_iam_role" "github_iam_role" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
           },
           StringLike = {
-            "token.actions.githubusercontent.com:sub" : "repo:ruchira088/*"
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:ruchira088/*",
+              "repo:ruchira088@5546272/*"
+            ]
           }
         }
       }
