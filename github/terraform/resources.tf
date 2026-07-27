a94295d01867ddcf9dd7ccd2d8df0b6186222ee7
Ruchira (Richie) Jayasekara <me@ruchij.com>
2026-07-27 14:16:23 +1000

    Fix `StringLike` syntax in GitHub Actions OIDC condition

--- github/terraform/resources.tf
+++ github/terraform/resources.tf
@@ -26,7 +26,7 @@ resource "aws_iam_role" "github_iam_role" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
           },
           StringLike = {
-            "token.actions.githubusercontent.com:sub" : [
+            "token.actions.githubusercontent.com:sub" = [
               "repo:ruchira088/*",
               "repo:ruchira088@5546272/*"
             ]
